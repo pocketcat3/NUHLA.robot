8e325fba2465bfcb686771fdc7b5ea147a37a997
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2023-06-04 11:55:42 +0300

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,6 +1,6 @@
 const int MSonar = A6;
-int datafromsensor = 0;
-float distance = 0;
+const int LSonar = A5;
+const int RSonar = A7;
 
 
 void setup() {
@@ -18,48 +18,63 @@ void setup() {
 }
 
 void loop() {
-  datafromsensor = analogRead(MSonar);
-  distance = 27, 86 * pow(datafromsensor, -1.15);// в сантиметрах
-  if (distance <= 5) {
-    turnright();
-  } else {
-    goforward();
-  }
-  Serial.println(distance);
+  if (analogRead(MSonar) <= 60 ) {
+     goforward();
+     if (analogRead(LSonar) >= 200) {
+        turnright();
+     }else if (analogRead(RSonar) >= 190){
+        turnleft();
+     }
+  } 
+           
+   
+ Serial.println(analogRead(LSonar));
+ Serial.println(analogRead(MSonar));
+ Serial.println(analogRead(RSonar));
+  Serial.println();
+ Serial.println();
+ delay(5000);
 }
 
 void goforward() {
-  analogWrite(10, 150);
+  analogWrite(10, 50);
   digitalWrite(9, 0);
   digitalWrite(8, 1);
   digitalWrite(7, 1);
   digitalWrite(6, 0);
-  analogWrite(5, 250);
+  analogWrite(5, 56);
 }
 
 void goback() {
-  analogWrite(10, 150);
+  analogWrite(10, 50);
   digitalWrite(9, 1);
   digitalWrite(8, 0);
   digitalWrite(7, 0);
   digitalWrite(6, 1);
-  analogWrite(5, 250);
+  analogWrite(5, 50);
 }
 
 void turnright() {
-  analogWrite(10, 150);
+  analogWrite(10, 50);
   digitalWrite(9, 1);
   digitalWrite(8, 0);
   digitalWrite(7, 1);
   digitalWrite(6, 0);
-  analogWrite(5, 250);
+  analogWrite(5, 50);
 }
 
 void turnleft() {
-  analogWrite(10, 150);
+  analogWrite(10, 50);
   digitalWrite(9, 0);
   digitalWrite(8, 1);
   digitalWrite(7, 0);
   digitalWrite(6, 1);
-  analogWrite(5, 250);
+  analogWrite(5, 50);
+}
+
+void stopMashine() {
+  digitalWrite(9, 0);
+  digitalWrite(8, 0);
+  digitalWrite(7, 0);
+  digitalWrite(6, 0);
 }
