d53a95afe8ada4bcb8b912aef3cb33d200f23b9b
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2023-06-18 11:58:52 +0300

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,35 +1,45 @@
-const int MSonar = A6;
-const int LSonar = A5;
-const int RSonar = A7;
+//const int MSonar = A6;
+//const int LSonar = A5;
+//const int RSonar = A7;
 
 
 void setup() {
   Serial.begin(9600);
   //right
   pinMode(5, OUTPUT);//speed
-  pinMode(9, OUTPUT);//nazad
+  pinMode(9, OUTPUT);//nazar
   pinMode(8, OUTPUT);//go
   //left
   pinMode(7, OUTPUT);//go
-  pinMode(6, OUTPUT);//nazad
+  pinMode(6, OUTPUT);//nazar
   pinMode(10, OUTPUT);//speed
 
-  
+
 }
 
 void loop() {
-  if (analogRead(A5) >= 180) {
-        turnright();
-     }else if (analogRead(A7) >= 160){
-        turnleft();
-     }
-  else  if (analogRead(A6) <= 20 ) {
-     goforward();
-     }
-  } 
-           
-   
-// Serial.println(analogRead(A5));
+  if (analogRead(A6) <= 50) {
+    goforward();
+  }
+  else if (analogRead(A5) >= 180 || (analogRead(A5) >= 180 && analogRead(A6) >= 50)) {
+    goback();
+    delay(150);
+    turnright();
+    delay(300);
+  }
+  else if (analogRead(A7) >= 160 || (analogRead(A7) >= 160 && analogRead(A6) >= 50)) {
+    goback();
+    delay(150);
+    turnleft();
+    delay(300);
+  }
+//  else if (analogRead(A6) >= 50 && analogRead(A7) < 160 && analogRead(A5) < 180) {
+//    turnleft();
+//  }
+}
+
+
+//Serial.println(analogRead(A5));
 //Serial.println(analogRead(A6));
 // Serial.println(analogRead(A7));
 //Serial.println("");
@@ -38,12 +48,12 @@ void loop() {
 
 
 void goforward() {
-  analogWrite(10, 55);
+  analogWrite(10, 50);
   digitalWrite(9, 0);
   digitalWrite(8, 1);
   digitalWrite(7, 1);
   digitalWrite(6, 0);
-  analogWrite(5, 50);
+  analogWrite(5, 55);
 }
 
 void goback() {
@@ -56,7 +66,7 @@ void goback() {
 }
 
 void turnright() {
-  analogWrite(10, 100);
+  analogWrite(10, 20);
   digitalWrite(9, 1);
   digitalWrite(8, 0);
   digitalWrite(7, 1);
@@ -70,7 +80,7 @@ void turnleft() {
   digitalWrite(8, 1);
   digitalWrite(7, 0);
   digitalWrite(6, 1);
-  analogWrite(5, 100);
+  analogWrite(5, 20);
 }
 
 void stopMashine() {
